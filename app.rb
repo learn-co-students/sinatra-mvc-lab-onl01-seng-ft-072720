d39e211edfdb9156d6840f728cc1da526fc297ea
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-09 19:15:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        piglatin = PigLatinizer.new
+        @piglatinize = piglatin.piglatinize(params[:phrase])
+        # "Hello world"
+        erb :piglatinize
+    end
+
+    # get '/piglatinize' do
+    #     erb :piglatinize
+    # end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,29 @@
+class PigLatinizer
+
+    def initialize
+    end
+
+    def piglatinize(string)
+        splitstring = string.split(" ")
+        splitstring.collect{|word| piglatinize_word(word)}.join(" ")
+    end
+
+    def piglatinize_word(word)
+        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+        return_value = ""
+
+        if word.length <=3
+            return_value = word+"way"
+        elsif vowels.include?(word[0])
+            return_value = word+"way"
+        elsif !vowels.include?(word[0]) && !vowels.include?(word[1]) && !vowels.include?(word[2])
+            return_value = word[3..-1]+word[0..2]+"ay"
+        elsif !vowels.include?(word[0]) && !vowels.include?(word[1])
+            return_value = word[2..-1]+word[0..1]+"ay"
+        else
+            return_value = word[1..-1]+word[0]+"ay"
+        end
+        return_value
+    end
+
+end
