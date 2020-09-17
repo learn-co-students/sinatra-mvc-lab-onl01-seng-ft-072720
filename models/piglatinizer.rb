85686f69e4058e84bc7cb82724d66a40154b7417
DavidFuents <Davidf0827@gmail.com>
2020-09-17 17:06:41 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,16 @@
+class PigLatinizer
+
+    def piglatinize(user_input)
+         result = user_input.split.map do |word|
+            if word.start_with?('a','e','i','o','u', 'A', 'E', 'I', 'O', 'U')
+                word + "way"
+            else 
+                parts = word.split(/([aeiou].*)/)
+                parts[1] + parts[0] + "ay"
+            end
+        end 
+        pig_latin = result.join(" ")
+        pig_latin
+    end
+
+end 
